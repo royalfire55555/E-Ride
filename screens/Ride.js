9b4740589ce9373a28c3c38fa618e36a31e4c34b
SammmE <101365537+SammmE@users.noreply.github.com>
2022-07-06 15:59:04 -0400

    added editable text box in ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -8,9 +8,13 @@ import {
     ImageBackground,
     Image,
     Alert,
+    ToastAndroid,
+    KeyboardAvoidingView,
 } from "react-native";
 import * as Permissions from "expo-permissions";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import firebase from "firebase";
+import db from "../config";
 
 const bgImage = require("../assets/background2.png");
 const appIcon = require("../assets/appIcon.png");
@@ -24,6 +28,9 @@ export default class RideScreen extends Component {
             domState: "normal",
             hasCameraPermissions: null,
             scanned: false,
+            bikeType: "",
+            userName: "",
+            bikeAssigned: "",
         };
     }
 
@@ -48,8 +55,128 @@ export default class RideScreen extends Component {
         });
     };
 
+    handleTransaction = async () => {
+        var { bikeId, userId } = this.state;
+        await this.getBikeDetails(bikeId);
+        await this.getUserDetails(userId);
+
+        db.collection("bicycles")
+            .doc(bikeId)
+            .get()
+            .then((doc) => {
+                var bike = doc.data();
+                if (bike.is_bike_available) {
+                    var { bikeType, userName } = this.state;
+
+                    this.assignBike(bikeId, userId, bikeType, userName);
+
+                    ToastAndroid.show(
+                        "You have rented the bike for next 1 hour. Enjoy your ride!!",
+                        ToastAndroid.SHORT
+                    );
+
+                    this.setState({
+                        bikeAssigned: true,
+                    });
+                } else {
+                    var { bikeType, userName } = this.state;
+
+                    this.returnBike(bikeId, userId, bikeType, userName);
+
+                    //For Android users only
+                    ToastAndroid.show(
+                        "We hope you enjoyed your ride",
+                        ToastAndroid.SHORT
+                    );
+
+                    this.setState({
+                        bikeAssigned: false,
+                    });
+                }
+            });
+    };
+
+    getBikeDetails = (bikeId) => {
+        bikeId = bikeId.trim();
+        db.collection("bicycles")
+            .where("id", "==", bikeId)
+            .get()
+            .then((snapshot) => {
+                snapshot.docs.map((doc) => {
+                    this.setState({
+                        bikeType: doc.data().bike_type,
+                    });
+                });
+            });
+    };
+
+    getUserDetails = (userId) => {
+        db.collection("users")
+            .where("id", "==", userId)
+            .get()
+            .then((snapshot) => {
+                snapshot.docs.map((doc) => {
+                    this.setState({
+                        userName: doc.data().name,
+                        userId: doc.data().id,
+                        bikeAssigned: doc.data().bike_assigned,
+                    });
+                });
+            });
+    };
+
+    assignBike = async (bikeId, userId, bikeType, userName) => {
+        //add a transaction
+        db.collection("transactions").add({
+            user_id: userId,
+            user_name: userName,
+            bike_id: bikeId,
+            bike_type: bikeType,
+            date: firebase.firestore.Timestamp.now().toDate(),
+            transaction_type: "rented",
+        });
+        //change bike status
+        db.collection("bicycles").doc(bikeId).update({
+            is_bike_available: false,
+        });
+        //change value  of bike assigned for user
+        db.collection("users").doc(userId).update({
+            bike_assigned: true,
+        });
+
+        // Updating local state
+        this.setState({
+            bikeId: "",
+        });
+    };
+
+    returnBike = async (bikeId, userId, bikeType, userName) => {
+        //add a transaction
+        db.collection("transactions").add({
+            user_id: userId,
+            user_name: userName,
+            bike_id: bikeId,
+            bike_type: bikeType,
+            date: firebase.firestore.Timestamp.now().toDate(),
+            transaction_type: "return",
+        });
+        //change bike status
+        db.collection("bicycles").doc(bikeId).update({
+            is_bike_available: true,
+        });
+        //change value  of bike assigned for user
+        db.collection("users").doc(userId).update({
+            bike_assigned: false,
+        });
+
+        // Updating local state
+        this.setState({
+            bikeId: "",
+        });
+    };
+
     render() {
-        const { bikeId, userId, domState, scanned } = this.state;
+        const { bikeId, userId, domState, scanned, bikeAssigned } = this.state;
         if (domState !== "normal") {
             return (
                 <BarCodeScanner
@@ -61,7 +188,7 @@ export default class RideScreen extends Component {
             );
         }
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
                 <View style={styles.upperContainer}>
                     <Image source={appIcon} style={styles.appIcon} />
                     <Text style={styles.title}>e-ride</Text>
@@ -71,6 +198,9 @@ export default class RideScreen extends Component {
                     <View style={styles.textinputContainer}>
                         <TextInput
                             style={[styles.textinput, { width: "82%" }]}
+                            onChangeText={(text) =>
+                                this.setState({ userId: text })
+                            }
                             placeholder={"User Id"}
                             placeholderTextColor={"#FFFFFF"}
                             value={userId}
@@ -81,11 +211,13 @@ export default class RideScreen extends Component {
                     >
                         <TextInput
                             style={styles.textinput}
+                            onChangeText={(text) =>
+                                this.setState({ bikeId: text })
+                            }
                             placeholder={"Bicycle Id"}
                             placeholderTextColor={"#FFFFFF"}
                             value={bikeId}
                         />
-
                         <TouchableOpacity
                             style={styles.scanbutton}
                             onPress={() => this.getCameraPermissions()}
@@ -93,8 +225,16 @@ export default class RideScreen extends Component {
                             <Text style={styles.scanbuttonText}>Scan</Text>
                         </TouchableOpacity>
                     </View>
+                    <TouchableOpacity
+                        style={[styles.button, { marginTop: 25 }]}
+                        onPress={this.handleTransaction}
+                    >
+                        <Text style={styles.buttonText}>
+                            {bikeAssigned ? "End Ride" : "Unlock"}
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 }
